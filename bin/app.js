4ab2e473c15f428fc14d784297a0cea17eb8d793
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-11-12 19:29:11 -0500

    learned about mounting and prepping autodrawing events in mithril

--- bin/app.js
+++ bin/app.js
@@ -95,7 +95,20 @@ module.exports = g;
 /***/ (function(module, exports, __webpack_require__) {
 
 var m = __webpack_require__(2);
-m.render(document.body, "hello world");
+
+var root = document.body;
+var count = 0;
+
+var hello = {
+  view: function() {
+    return m("main", [
+      m("h1", {class: "title"}, "My first app"),
+      m("button", {onclick: function() {count++}}, count + " clicks")
+    ])
+  }
+}
+
+m.mount(root, hello);
 
 
 /***/ }),
--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 var m = require("mithril");
-m.render(document.body, "hello world");
+
+var root = document.body;
+var count = 0;
+
+var hello = {
+  view: function() {
+    return m("main", [
+      m("h1", {class: "title"}, "My first app"),
+      m("button", {onclick: function() {count++}}, count + " clicks")
+    ])
+  }
+}
+
+m.mount(root, hello);
